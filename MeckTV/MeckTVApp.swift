b609e5c1d9c42302803b9ab4383467e1d0cff764
Asher Pope <asherpope@gmail.com>
2023-01-26 05:37:26 -0500

    Initial Commit

---
+++ MeckTV/MeckTVApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeckTVApp.swift
+//  MeckTV
+//
+//  Created by Asher Pope on 1/26/23.
+//
+
+import SwiftUI
+
+@main
+struct MeckTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
